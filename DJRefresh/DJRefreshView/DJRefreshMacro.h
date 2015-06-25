601d4a88d578b943bd803dca4b79ebd79fb0405e
ydj <yudejian@163.com>
2015-06-25 11:43:50 +0800

    add refresh macro

---
+++ DJRefresh/DJRefreshView/DJRefreshMacro.h
@@ -0,0 +1,13 @@
+//
+//  DJRefreshMacro.h
+//  DJRefreshSample
+//
+//  Created by YuDejian on 15/6/25.
+//  Copyright (c) 2015年 ydj. All rights reserved.
+//
+
+#ifndef DJRefreshSample_DJRefreshMacro_h
+#define DJRefreshSample_DJRefreshMacro_h
+
+
+#endif
